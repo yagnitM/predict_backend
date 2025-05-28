932782cba36b47649b62deecf7bcbf1c10830cea
Yagnit <yagnitmahajan881@gmail.com>
2025-05-28 10:11:33 +0530

    app.py updated

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ app = FastAPI()
 # CORS config for frontend calls
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",
-                   "https://acepredictor-frontend.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
